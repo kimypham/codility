8f1b44237ae9fe25d58af25e88fc11249e987e93
Kim Pham <km.phm2@gmail.com>
2024-08-14 21:11:25 +1000

    retry nesting

--- 21-nesting.ts
+++ 21-nesting.ts
@@ -1,23 +1,23 @@
-// link to codility report: https://app.codility.com/demo/results/trainingD4WMZA-DXD/
+// link to codility report: https://app.codility.com/demo/results/trainingSPDAN2-4JY/
 
 function solution(S: string): number {
     const stack: string[] = [];
 
     for (let i = 0; i < S.length; i++) {
         if (S[i] == '(') {
-            stack.push(S[i]);
+            stack.push('(');
         } else {
-            if (stack.length > 0) {
-                stack.pop();
+            if (stack.length === 0) { // if stack is empty, that means there wasn't a left bracket '(' to match the right bracket / there was an extra right bracket 
+                return 0;
             } else {
-                return 0; // if the stack was empty and we saw a ')', that means that there wasn't a '(' to match it
+                stack.pop();
             };
         };
     };
 
-    if (stack.length === 0) { // we shouldn't have any extra '(' in the stack
-        return 1;
-    };
+    if (stack.length !== 0) { // if stack is not empty, that means there was an extra left bracket '('
+        return 0;
+    }
 
-    return 0;
+    return 1;
 };
